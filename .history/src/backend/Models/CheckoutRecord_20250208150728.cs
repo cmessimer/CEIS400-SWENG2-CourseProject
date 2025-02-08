34d4f73dbbb488604b0a8dd0302932f20d5ab8f4
chadmessimer <103394173+cmessimer@users.noreply.github.com>
2025-02-08 15:19:06 -0500

    Initial Code Push

---
+++ .history/src/backend/Models/CheckoutRecord_20250208150728.cs
@@ -0,0 +1,9 @@
+public class CheckoutRecord
+{
+    [Key]
+    public int RecordID { get; set; }
+    public int UserID { get; set; }
+    public int EquipmentID { get; set; }
+    public DateTime CheckoutDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+}
